9b25e99cc96810dd7c82b25518cce601c9c7fd0b
Bayu Aditya <adityabayu170@gmail.com>
2024-08-09 20:19:37 +0700

    not finished+3

---
+++ app/src/main/java/com/example/projectkp/laguhindia.kt
@@ -0,0 +1,20 @@
+package com.example.projectkp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class laguhindia : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_laguhindia)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
